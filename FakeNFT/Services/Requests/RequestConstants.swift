b9580c57746c45fca5b3b0ddeb472147e86716e7
Давид <bekoevdawid@yandex.ru>
2025-03-25 15:33:04 +0300

    токен

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,4 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-    static let token = "59db9adc-fa26-4e11-9e0d-0cd1ccdb351"
+    static let token = "59db9adc-fa26-4e11-9e0d-0cd1ccdb3512"
 }
