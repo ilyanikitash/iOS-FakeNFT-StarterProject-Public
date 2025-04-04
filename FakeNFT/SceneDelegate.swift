b1842bf095898fea2ab27e259a8074b0a0ff680d
Давид <bekoevdawid@yandex.ru>
2025-04-04 15:11:38 +0300

    отступы

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -14,22 +14,22 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
-           sendLikesToServer()
-       }
-
-       func sceneDidEnterBackground(_ scene: UIScene) {
-           sendLikesToServer()
-       }
-
-       private func sendLikesToServer() {
-           let likes = LikesStorageImpl.shared.getAllLikes()
-           servicesAssembly.profileService.updateLikes(likes: likes) { result in
-               switch result {
-               case .success:
-                   print("Лайки успешно отправлены перед выходом из приложения.")
-               case .failure(let error):
-                   print("Ошибка отправки лайков: \(error.localizedDescription)")
-               }
-           }
-       }
+        sendLikesToServer()
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        sendLikesToServer()
+    }
+    
+    private func sendLikesToServer() {
+        let likes = LikesStorageImpl.shared.getAllLikes()
+        servicesAssembly.profileService.updateLikes(likes: likes) { result in
+            switch result {
+            case .success:
+                print("Лайки успешно отправлены перед выходом из приложения.")
+            case .failure(let error):
+                print("Ошибка отправки лайков: \(error.localizedDescription)")
+            }
+        }
+    }
 }
