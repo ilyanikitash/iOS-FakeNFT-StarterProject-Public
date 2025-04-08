39ea597d4a4c808c7fab2338a423dcad6892d470
KirillDrobin <krldrn@icloud.com>
2025-04-08 18:22:36 +0200

    Исправление конфликта TabBarController

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -15,19 +15,47 @@ final class TabBarController: UITabBarController {
         image: UIImage(named: "cart"),
         tag: 0
     )
+    
+    private let profileTabBarItem = UITabBarItem(
+        title: "Профиль",
+        image: UIImage(named: "Profile"),
+        tag: 0
+    )
+    private let statisticTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.statistic", comment: ""),
+        image: UIImage(named: "tab_statistic"),
+        tag: 1)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let profileViewController = ProfileViewController(servicesAssembly: servicesAssembly)
+        profileViewController.tabBarItem = profileTabBarItem
+        let profileNavController = UINavigationController(rootViewController: profileViewController)
+        
+        let profileImage = UIImage(named: "Profile")?.withRenderingMode(.alwaysOriginal)
+        let resizedImage = profileImage?.resized(to: CGSize(width: 30, height: 30))
+        profileTabBarItem.image = resizedImage
+        
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
+        
+        let statisticUsersListVC = StatisticUsersListViewController()
+        let statisticUsersListVCNavController = UINavigationController(rootViewController: statisticUsersListVC)
+        statisticUsersListVCNavController.setNavigationBarHidden(false, animated: false)
+        
         catalogController.tabBarItem = catalogTabBarItem
         
         let cartController = UINavigationController(rootViewController: CartMainViewController())
         cartController.tabBarItem = cartTabBarItem
 
-        viewControllers = [catalogController, cartController]
+        statisticUsersListVCNavController.tabBarItem = statisticTabBarItem
+
+        viewControllers = [catalogController]
+        viewControllers?.append(statisticUsersListVCNavController)
+        viewControllers?.append(profileNavController)
+        
         view.backgroundColor = .systemBackground
     }
 }
